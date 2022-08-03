24ad79bcc3a9a45c4221262460f5f6658c3235dd
Emma Pierson <2plus2make5@gmail.com>
2022-08-03 14:33:23 -0400

    make refresh work automatically

--- print_availability.py
+++ print_availability.py
@@ -51,13 +51,10 @@ def main():
     if os.path.exists(os.path.join(BASE_PATH, 'token.json')):
         creds = Credentials.from_authorized_user_file(os.path.join(BASE_PATH, 'token.json'), SCOPES)
     # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                os.path.join(BASE_PATH, CREDENTIALS_NAME_DOWNLOADED_FROM_GOOGLE), SCOPES)
-            creds = flow.run_local_server(port=0)
+    if (not creds) or (not creds.valid) or (creds and creds.expired and creds.refresh_token):
+        flow = InstalledAppFlow.from_client_secrets_file(
+            os.path.join(BASE_PATH, CREDENTIALS_NAME_DOWNLOADED_FROM_GOOGLE), SCOPES)
+        creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open(os.path.join(BASE_PATH, 'token.json'), 'w') as token:
             token.write(creds.to_json())
